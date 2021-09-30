bd41da1d04f751781a443b2d79253653407d1eb3
huang0308 <209410215@gms.tku.edu.tw>
2021-09-30 20:25:52 +0800

    first commit

---
+++ src/App_15.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Todo_15 from './components/Todo_15'
+
+function App_15() {
+  return (
+    <div>
+    <h1>My Todos</h1>
+      <Todo_15 text ='Learn React' />
+      <Todo_15 text ='Learn QwQ' />
+      <Todo_15 text ='Learn QoQ' />
+  </div>
+  );
+}
+
+export default App_15;
