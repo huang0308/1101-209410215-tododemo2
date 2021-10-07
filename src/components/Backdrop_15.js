ee34a995ba834544cc398faa38a1801502afa8fb
huang0308 <209410215@gms.tku.edu.tw>
2021-10-07 22:08:04 +0800

    test

---
+++ src/components/Backdrop_15.js
@@ -0,0 +1,8 @@
+function Backdrop_15(props) {
+    return (
+        <div className="backdrop" onClick={props.onClick}></div>
+    )
+
+}
+
+export default Backdrop_15;
--- src/components/Todo_15.js
+++ src/components/Todo_15.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+import Modal_15 from '../components/Modal_15'
+import Backdrop_15 from '../components/Backdrop_15'
+
 function Todo_15(props){
+    const [showModal, setShowModal ] = useState();
+
+    function showModalHandler(){
+        setShowModal(true);
+    }
+
+    function closeModalHandler(){
+        setShowModal(false);
+    }
     return(
         <div>
             <div className="card">
                 <h2>{props.text}</h2>
                 <div className="actions">
-                    <button className="btn">Delete</button>
+                    <button className="btn" onClick={showModalHandler}>Delete</button>
                 </div>
             </div>
+            {showModal && <Backdrop_15 onClick={closeModalHandler}/>}
+            {showModal && <Modal_15 text='Are you sure?' onClose={closeModalHandler}/>}
         </div>
+
     );
 }
 
